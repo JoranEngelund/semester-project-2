1897993d74717be7f106afc613e57d232a54d774
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2023-02-20 15:00:28 +0100

    Update search-length-checker.mjs

--- src/js/length-checkers/search-length-checker.mjs
+++ src/js/length-checkers/search-length-checker.mjs
@@ -1,12 +1,12 @@
 /**
- * Checks the length of the listings array and displays a message if it is empty.
- * @param {Array} listings - The array of listings to be checked
+ * Checks the length of the filtered listings and displays a message if there are no results.
+ * @param {Array<Object>} listings -  An array of objects representing the filtered listings to check.
  * @returns {void}
  * @example
- * listingsLengthChecker(listings);
+ * searchLengthChecker(listings);
  * @description
- * - The listingsLengthChecker function checks the length of the listings array and displays a message to the users if it is empty.
- * - It takes an array of listings as its parameter. If the length of the array is less than or equal to zero, it clears the inner HTML of the element with an id of listing-container and replaces it with an error message and a button to refresh the page. The function does not return anything.
+ * - The searchLengthChecker function takes in an array of objects representing the filtered listings to check. It checks the length of the listings array and displays a message if there are no results..
+ * - The function first checks if the length of the listings array is less than or equal to zero. If it is, it then retrieves the listings container using the querySelector method, clears its contents using the innerHTML property, and replaces it with a new HTML element that displays a message indicating that there were no listings found based on the search result. The message is displayed in a <div> element with centered text and a <p> element with the text "We can't find any listings based on your search result".
  */
 export function searchLengthChecker(listings) {
   if (listings.length <= 0) {
